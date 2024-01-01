f0779bf07e5faef2c496b096f484097b2b44257a
AphroDen <hamglawl@gmail.com>
2024-01-02 00:07:36 +0200

    testing

--- pwdemo/pwdemo/spiders/pwspider.py
+++ pwdemo/pwdemo/spiders/pwspider.py
@@ -1,6 +1,5 @@
-from typing import Iterable
-
 import scrapy
+from scrapy_playwright.page import PageMethod
 from scrapy import Request
 
 
@@ -9,10 +8,15 @@ class PwspiderSpider(scrapy.Spider):
 
     def start_requests(self):
         yield scrapy.Request('https://shoppable-campaign-demo.netlify.app/#/',
-                             meta={'playwright': True})
+                             meta=dict(
+                                 playwright=True,
+                                 playwright_include_page=True,
+                                 playwright_page_coroutines=[
+                                     PageMethod('wait_for_selector', 'div#productListing')
+                                 ]
+                             ))
 
-    def parse(self, response):
+    async def parse(self, response):
         yield {
             'text': response.text,
-            'lolkep': response.text,
         }
